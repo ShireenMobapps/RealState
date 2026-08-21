70ff222abd810ab2697370f050b8051a488fabc5
ShireenMobapps <mobappssolutions187@gmail.com>
2026-08-21 17:49:05 +0530

    7th commit

--- AIPoweredRealEstate/API/Constant.swift
+++ AIPoweredRealEstate/API/Constant.swift
@@ -10,5 +10,6 @@ import Foundation
 enum Constant {
     /// Replace with your Maps SDK for iOS key from Google Cloud Console.
     static let googleMapsAPIKey = "AIzaSyAaAOACcSdZsf0MJnQ3O6vQtMGxvP8kMCY"
-    static let googleMapsAPIKey2 = "AIzaSyAaAOACcSdZsf0MJnQ3O6vQtMGxvP8kMCY"
+    
+   
 }
