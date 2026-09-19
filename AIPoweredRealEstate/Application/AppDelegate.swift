aa682d5405aa6c9bf75f128f31dfde8dfbf9a45b
ShireenMobapps <mobappssolutions187@gmail.com>
2026-09-19 16:51:01 +0530

    saif

--- AIPoweredRealEstate/Application/AppDelegate.swift
+++ AIPoweredRealEstate/Application/AppDelegate.swift
@@ -30,7 +30,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         var arr2:[Int] = [1,2,3,4]
         print(arr2)
         
-        print("I am Shireen")
+        print("I am Saif")
         
         
         
