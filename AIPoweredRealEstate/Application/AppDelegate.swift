c0572d3b469f7ed553502bc1457499da572c1fbe
ShireenMobapps <mobappssolutions187@gmail.com>
2026-09-19 16:56:33 +0530

    hh

--- AIPoweredRealEstate/Application/AppDelegate.swift
+++ AIPoweredRealEstate/Application/AppDelegate.swift
@@ -30,10 +30,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         var arr2:[Int] = [1,2,3,4]
         print(arr2)
         
-        print("I am Saif")
-        
-        
-        
+      
         
         return true
     }
